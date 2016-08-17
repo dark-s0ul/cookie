f2fb40cddcc95efc1f9a414510b8d80ae7c0fe1c
Max Pasichnyk <maximmkaaa@gmail.com>
2016-08-17 20:20:34 +0300

    Update bf.h

--- bf.h
+++ bf.h
@@ -2,32 +2,32 @@
 #include <string>
 #include <vector>
 
-namespace bf{
+namespace bf {
 	std::vector<char> mem(30000);
 	auto p = mem.begin();
-	
-	void eval(std::string str){
+
+	void eval(std::string str) {
 		auto s = str.begin();
-		while(s != str.end()){
-			if(*s == '>' && p != mem.end()) p++;
-			if(*s == '<' && p != mem.begin()) p--;
-			if(*s == '+') ++*p;
-			if(*s == '-') --*p;
-			if(*s == '.') putc(*p, stdout);
-			if(*s == ',') *p = getc(stdin);
-			if(*s == '['){
+		while (s != str.end()) {
+			if (*s == '>' && p != mem.end()) p++;
+			else if (*s == '<' && p != mem.begin()) p--;
+			else if (*s == '+')++*p;
+			else if (*s == '-')--*p;
+			else if (*s == '.') putc(*p, stdout);
+			else if (*s == ',') *p = getc(stdin);
+			else if (*s == '[') {
 				int br = 0;
-				while(!*p){
-					if(*s == '[') br++;
-					if(*s == ']' && br-- == 1) break;
+				while (!*p) {
+					if (*s == '[') br++;
+					if (*s == ']' && br-- == 1) break;
 					s++;
 				}
 			}
-			if(*s == ']'){
+			else if (*s == ']') {
 				int br = 0;
-				while(*p){
-					if(*s == ']') br++;
-					if(*s == '[' && br-- == 1) break;
+				while (*p) {
+					if (*s == ']') br++;
+					if (*s == '[' && br-- == 1) break;
 					s--;
 				}
 			}
