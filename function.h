5f7eac239897617ea8c8c9088d7214d1014b4d27
Max Pasichnyk <maximmkaaa@gmail.com>
2018-02-04 19:44:59 +0200

    Update function.h

--- function.h
+++ function.h
@@ -7,6 +7,7 @@ template<typename R, typename... U>
 struct function <R(U...)> {
 private:
 	struct IHolder {
+		int refs;
 		virtual R invoke(U... args) = 0;
 	};
 
@@ -14,27 +15,53 @@ private:
 	struct Holder : IHolder {
 		T method;
 
-		Holder(const T &method) : method(method) {}
+		inline Holder(const T &method) : method(method) {
+			IHolder::refs = 1;
+		}
 
 		inline virtual R invoke(U... args) override {
 			return method(args...);
 		}
 	};
 
-	unique_ptr<IHolder> holder;
+	IHolder *holder;
 
 public:
-	function() : holder() {}
+	inline function() : holder() {}
+
+	inline function(const function<R, U...> &f) {
+		holder = f.holder;
+		if (holder) {
+			holder->refs++;
+		}
+	}
+
+	inline function <R, U...> &operator =(const function<R, U...> &f) {
+		if (holder && --holder->refs == 0) {
+			delete holder;
+		}
+		holder = f.holder;
+		if (holder) {
+			holder->refs++;
+		}
+		return *this;
+	}
 
 	template <typename T>
-	function(const T &method) : holder(new Holder<T>(method)) {}
+	inline function(const T &method) : holder(new Holder<T>(method)) {}
 
-	function(R(*method)(U...)) : function([=](U... args) -> R { return method(args...); }) {}
+	inline function(R(*method)(U...)) : function([=](U... args) -> R { return method(args...); }) {}
 
 	template <class T>
-	function(T *pointer, R(T::*method)(U...)) : function([=](U... args) -> R { return (pointer->*method)(args...); }) {}
+	inline function(T *pointer, R(T::*method)(U...)) : function([=](U... args) -> R { return (pointer->*method)(args...); }) {}
 
-	R operator()(U... args) const {
+	inline ~function() {
+		if (--holder->refs == 0) {
+			delete holder;
+		}
+	}
+
+	inline R operator()(U... args) const {
 		return holder ? holder->invoke(args...) : R();
 	}
 };
