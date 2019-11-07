02e0453058caf5e1f97aa211606c6e392afcba57
Max Pasichnyk <maximmkaaa@gmail.com>
2019-11-07 12:57:44 +0200

    Update property.h

--- property.h
+++ property.h
@@ -1,67 +1,103 @@
 #pragma once
 
+#include <functional>
+
+template <typename T = void, template <typename> typename...>
+struct property;
+
+template <>
+struct property<> {
+    template <typename T>
+    using get = std::function<T()>;
+
+    template <typename T>
+    using set = std::function<void(const T&)>;
+};
+
 template <typename T>
-struct property {
+struct property<T> {
+    using value_type = std::decay_t<T>;
+
+    property(value_type& value) : m_value(value) {}
+    property(value_type&& value) : m_value(std::forward<T>(value)) {}
+
+    operator value_type&() {
+        return m_value;
+    }
+
+    operator const value_type&() const {
+        return m_value;
+    }
+
+    property& operator =(value_type& v) {
+        m_value = v;
+        return *this;
+    }
+
+    property& operator =(value_type&& v) {
+        m_value = std::forward<T>(v);
+        return *this;
+    }
 private:
-	template<typename R>
-	struct accessor;
+    T m_value;
+};
 
-	template<typename R, typename... U>
-	struct accessor <R(U...)> {
-	private:
-		struct method {
-			virtual R invoke(U... args) = 0;
-		};
+template <typename T>
+struct property<T, property<>::get> {
+    template <typename U>
+    property(U&& get) : m_get(get) {}
 
-		template <typename M>
-		struct holder : method {
-			M func;
+    operator T() {
+        return m_get();
+    }
 
-			inline holder(const M &func) : func(func) {}
+    operator const T() const {
+        return m_get();
+    }
 
-			inline virtual R invoke(U... args) override {
-				return func(args...);
-			}
-		};
+private:
+    const property<>::get<T> m_get;
+};
 
-		method *func;
+template <typename T>
+struct property<T, property<>::set> {
+    template <typename U>
+    property(U&& get) : m_set(get) {}
 
-	public:
-		template <typename M>
-		inline accessor(const M &func) : func(new holder<M>(func)) {}
+    property& operator =(const T& v) {
+        m_set(v);
+        return *this;
+    }
 
-		inline ~accessor() {
-			delete func;
-			func = 0;
-		}
+    property& operator =(T&& v) {
+        m_set(std::forward<T>(v));
+        return *this;
+    }
 
-		inline R operator()(U... args) const {
-			return func->invoke(args...);
-		}
-	};
+private:
+    const property<>::set<T> m_set;
+};
 
-	accessor<T()> get;
-	accessor<void(const T&)> set;
+template <typename T>
+struct property<T, property<>::get, property<>::set> {
+    const property<>::get<T> get;
+    const property<>::set<T> set;
 
-public:
-	inline property(const auto &get, const auto& set) : get(get), set(set) {}
+    operator T() {
+        return get();
+    }
 
-	inline property(property&&) = delete;
+    operator const T() const {
+        return get();
+    }
 
-	inline property<T>& operator=(const property<T>& prop) {
-		return *this;
-	}
+    property& operator =(const T& v) {
+        set(v);
+        return *this;
+    }
 
-	inline property<T>& operator=(const T& value) {
-		set(value);
-		return *this;
-	}
-
-	inline operator T() {
-		return get();
-	}
-
-	inline operator const T() const {
-		return get();
-	}
+    property& operator =(T&& v) {
+        set(std::forward<T>(v));
+        return *this;
+    }
 };
